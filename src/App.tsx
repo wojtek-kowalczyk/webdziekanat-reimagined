66e2012789602850ed5bf2596b8deb96d75d461a
Wojtek Kowalczyk <wojtekkowalczyk01@gmail.com>
2022-11-13 11:09:21 +0100

    test dialog + notes

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import { Link, Route, Routes } from "react-router-dom"
 import SignIn from './components/signin';
 import Home from './components/home';
 import PageNotFound from './components/page-not-found';
-import { createTheme, IconButton, PaletteMode, ThemeOptions, ThemeProvider } from '@mui/material';
+import { createTheme, CssBaseline, IconButton, PaletteMode, ThemeOptions, ThemeProvider } from '@mui/material';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
-import { amber, deepOrange, grey } from '@mui/material/colors';
 import { getDesignTokens } from './theme';
+import SimpleDialogDemo from './components/testdialo';
 
 const ColorModeContext = React.createContext({ toggleColorMode: () => { } });
 
@@ -35,19 +35,32 @@ function App() {
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
-        <div className='header'>
+      <CssBaseline />
+
+        <SimpleDialogDemo/>
+
+        <div className='side-nav' style={{ backgroundColor: "#ff00f0" }}>
+          <p>naviation menu</p>
+          <ul className='side-nav-list'>
+            <li><Link to=''>Home</Link></li>
+            <li><Link to='signin'>Sign in</Link></li>
+            <li><Link to='whatever'>whatever</Link></li>
+          </ul>
+        </div>
+
+        <div className='content'>
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/signin' element={<SignIn />} />
             <Route path='*' element={<PageNotFound />} />
           </Routes>
-        </div>
-        <div className='content'>
+
           <p>default content</p>
           <IconButton onClick={colorMode.toggleColorMode} color="inherit">
             {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
           </IconButton>
         </div>
+
       </ThemeProvider>
     </ColorModeContext.Provider>
   );
